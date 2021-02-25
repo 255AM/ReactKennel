ba5fc51d4f860c113607964e6879de4bdaf12bc3
Chance Lane <lanecw@gmail.com>
2021-02-25 13:32:44 -0500

    first commit

---
+++ src/components/customer/CustomerCard.js
@@ -0,0 +1,8 @@
+import React from "react"
+import "./Customer.css"
+
+export const CustomerCard = () => (
+    <section className="customer">
+        <h3 className="customer__name">Jim</h3>
+    </section>
+)
---
+++ src/components/location/LocationCard.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Location.css"
+
+export const LocationCard = () => (
+    <section className="location">
+        <h3 className="location__name">here</h3>
+        
+    </section>
+)
